2471593847813eef945ced16d1181d151d8a5c76
kowais915 <kowais915@gmail.com>
2023-07-10 23:39:45 +0500

    :construction: Added a font familty form google fonts

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+
+
+const Navbar = () => {
+    return ( 
+
+
+        <nav>
+            <div className="brand">
+                <h2>SpaceX Mania</h2>
+            </div>
+            
+            <div className="tabs">
+                <div className="Vehicles">
+                    <button>Launch Vehicles</button>
+                </div>
+
+                <div className="Vehicles">
+                    <button>Launchpads</button>
+                </div>
+            </div>
+
+        </nav>
+        
+    
+   );
+}
+ 
+export default Navbar;
