107a6f8d65ff961c04afd4887a3a562a245aa0ea
kowais915 <kowais915@gmail.com>
2023-07-12 03:55:09 +0500

    :technologist: Got React Query Dev Tools to work

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import Vehicles from './components/Vehicles';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {useState} from 'react';
 import {useQuery, QueryClient, QueryClientProvider} from '@tanstack/react-query';
-
-
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
   const [page, setPage] = useState('vehicles');
@@ -51,11 +50,12 @@ function App() {
 
       
 
-
+ 
 
       
 
     </div>
+     <ReactQueryDevtools/>
     </QueryClientProvider>
   );
 }
